bf6deb8b0d80693169e6569325adf2dace31af25
mavulag <mavulageofrey1@gmail.com>
2023-12-05 13:46:44 +0300

    Adding Task class model

--- backend/todo/models.py
+++ backend/todo/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Task(models.Model):
+    taskId = models.AutoField(primary_key=True)
+    Title = models.CharField(max_length=250)
+    Description = models.TextField()
